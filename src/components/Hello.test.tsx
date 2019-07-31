e3563aff37af17d5613c41e3b688b0616c59fd93
1225zhangqian <1139384196@qq.com>
2019-07-31 11:42:20 +0800

    change to  yarn create react-app my-app --typescript

---
+++ src/components/Hello.test.tsx
@@ -0,0 +1,15 @@
+import * as enzyme from "enzyme";
+import * as React from "react";
+import Hello from "./Hello";
+// import Adapter from "enzyme-adapter-react-16";
+
+// enzyme.configure({ adapter: new Adapter() });
+it("renders the correct text when no enthusiasm level is given", () => {
+  const hello = enzyme.shallow(<Hello name="Daniel" />);
+  expect(hello.find(".greeting").text()).toEqual("Hello Daniel!");
+});
+
+it("renders the correct text with an explicit enthusiasm of 1", () => {
+  const hello = enzyme.shallow(<Hello name="Daniel" enthusiasmLevel={1} />);
+  expect(hello.find(".greeting").text()).toEqual("Hello Daniel!");
+});
